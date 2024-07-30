9fcd0c2a7bc48e284228fe6a27089000be3c8847
anmol <anmolof10gms@gmail.com>
2024-07-31 00:05:37 +0530

    corrected some config issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -36,7 +37,7 @@ const Footer = () => {
         <div className="flex items-center md:gap-3 gap-6">
 {socialMedia.map((profile) => (
     <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-        <a href="https://www.instagram.com/editbyaadi/" target="_blank"><img src={profile.img} alt={profile.img} width={20} height={20} className="bg-white-100 rounded" />
+        <a href="https://www.instagram.com/editbyaadi/" target="_blank"><Image src={profile.img} alt={profile.img} width={20} height={20} className="bg-white-100 rounded" />
         </a>
     </div>
 ))}
